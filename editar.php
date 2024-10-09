ee78045c6a58fe1e31c365afa8e9f1124516fae1
Suelen Souza <suelensouzasz14@gmail.com>
2024-10-08 21:01:53 -0300

    editar

--- editar.php
+++ editar.php
@@ -14,5 +14,30 @@ if(isset($_GET['id'])){
         exit;
     }
 }
-if()
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $nome= $_POST['nome'];
+    $descricao= $_POST['descricao'];
+    $preco= $_POST['preco'];
+    $quantidade= $_POST['quantidade'];
+
+    $sql="UPDATE produtos SET nome=:nome,descricao=:descricao,preco=:preco,quantidade=:quantidade WHERE id=:id";
+    $sql = $pdo->prepare($sql);
+    $sql->execute(params: [
+        ":nome"=> $nome,
+        "descricao"=> $descricao,
+        "preco"=> $preco,
+        "quantidade"=> $quantidade,
+        "id"=> $id
+    ] );
+    echo"Produto atualizado com sucesso";
+}
 ?>
+
+<form action: "editar.php" method:"post" >
+        Nome: <input type="text" id="nome" name="nome"><
+        Descricao:<input type="text" id="descricao" name="descricao">
+        Preço: <input type="text" id="preco" name="preco">
+        Quantidade: <input type="number" id="quantidade" name="quantidade"><br>
+
+        <input type="submit" value="Atualizar Produto">        
+    </form>
